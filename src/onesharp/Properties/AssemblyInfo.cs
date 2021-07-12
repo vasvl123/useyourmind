3685fc98db2a2254d01b99615fe4b9aed791fdf9
vasvl123 <vasvl123@yandex.ru>
2021-07-12 14:37:10 +0300

    fetch

--- src/onesharp/Properties/AssemblyInfo.cs
+++ src/onesharp/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Onesharp")]
+[assembly: AssemblyTitle("onesharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
