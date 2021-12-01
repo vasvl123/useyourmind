b5bcb59665090a32ba2bd8fe814fe857385a6e75
vasvl123 <vasvl123@yandex.ru>
2021-12-01 22:29:04 +0200

    slovar

--- src/parse/Program.cs
+++ src/parse/Program.cs
@@ -15,7 +15,8 @@ namespace parse
         {
             var app = new parse();
             app.АргументыКоманднойСтроки = args;
-            app.parse_ruez();
+            //app.parse_ruez();
+            app.parse_slovar();
         }
     }
 }
